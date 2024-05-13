65ecc99097be7b369b2d797ba86d25325704ccf4
Bella2301 <bethlehemwoldekidan89@gmail.com>
2024-05-13 17:57:02 +0300

    six.java

--- labexam6/six.java
+++ labexam6/six.java
@@ -1,48 +1,32 @@
 package labexam6;
 
-public class six {
+public class Six {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int [] array = {3,7,1,9,4};
-		deleteElement(array,1);
-	
-		
-		
-		for(int num: array) {
-			System.out.print( array + " ");
-		}
+    public static void main(String[] args) {
+        int[] originalArray = {3, 7, 1, 9, 4};
+        deleteElement(originalArray, 1); // Deleting element at index 1
 
-		
-		
-		//yaredmgmt@gmail.com
-		
-	}
-
-
-public static int[] deleteElement(int[] array, int index) {
-    if (index < 0 || index >= array.length) {
-        System.out.println("Invalid index.");
-        return array;
-    }
-    int[] newArray = new int[array.length - 1];
-    int newIndex = 0;
-    for (int i = 0; i < array.length; i++) {
-        if (i != index) {
-            newArray[newIndex++] = array[i];
+        System.out.println("Modified Array:");
+        for (int num : originalArray) {
+            System.out.print(num + " ");
         }
     }
-    return newArray;
-}
-}
 
-
-/*Consider an array of integers in Java, named array, containing elements as 
-follows: {3, 7, 1, 9, 4}. Write a Java method named deleteElement that 
-takes two parameters: the array and an integer representing the index of the 
-element to delete. 
-The method should delete the element at the specified index from the array 
-and return the modified array. If the index is invalid (less than 0 or greater 
-than or equal to the array length), the method should print "Invalid index." 
-and return the original array unchanged.*/
- 
+    public static void deleteElement(int[] array, int index) {
+        if (index < 0 || index >= array.length) {
+            System.out.println("Invalid index.");
+            return;
+        }
+        int[] newArray = new int[array.length - 1];
+        int newIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i != index) {
+                newArray[newIndex++] = array[i];
+            }
+        }
+        
+        for (int i = 0; i < newArray.length; i++) {
+            array[i] = newArray[i];
+        }
+    }
+}
